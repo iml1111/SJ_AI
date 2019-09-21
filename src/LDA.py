20406b60820b448ea7660a73cb47c08df40ca7d8
iml1111 <shin10256@gmail.com>
2019-09-21 19:39:48 +0900

    refac

--- src/LDA.py
+++ src/LDA.py
@@ -25,13 +25,14 @@ import platform
 warnings.filterwarnings('ignore')
 # HyperParameter
 #총 토픽 수
-NUM_TOPICS = 10
+NUM_TOPICS = 7
 # 많을수록 곱씹어서 봄
 PASSES = 20
-# 학습을 수행할 병렬 워커 수
-WORKERS = 4
 # 학습에 포함될 최소 글자수
 POST_LIMIT = 15
+# 학습을 수행할 병렬 워커 수
+WORKERS = 4
+
 #환경에 따라 변경 필요
 os_platform = platform.platform()
 if os_platform.startswith("Windows"):
@@ -41,7 +42,8 @@ else:
 	model_path = "/home/iml/model/lda_output/soojle_lda_model"
 	dict_path = "/home/iml/model/lda_output/soojle_lda_dict"
 
-def learn(col, start = 0, count = None, split_doc = 1, update = False):
+## DB 내의 데이터 모델 코퍼스로 만들기
+def make_corpus(col, start = 0, count = None, split_doc = 1, update = False):
 	corpus = []
 	dictionary = corpora.Dictionary()
 	idx = 1 + start
@@ -62,6 +64,11 @@ def learn(col, start = 0, count = None, split_doc = 1, update = False):
 		dictionary.add_documents(tokenized_doc)
 		corpus += [dictionary.doc2bow(text) for text in tokenized_doc]
 		idx += split_doc
+	return corpus, dictionary
+
+
+## 코퍼스를 통해 학습 수행
+def learn(corpus, dictionary, update = False):
 	print("Training...")
 	ldamodel = LdaMulticore(
 				corpus,
@@ -70,46 +77,22 @@ def learn(col, start = 0, count = None, split_doc = 1, update = False):
 				passes = PASSES,
 				workers = WORKERS
 				)
-	cm = CoherenceModel(model=model, corpus=corpus, coherence='u_mass')
+	cm = CoherenceModel(model=ldamodel, corpus=corpus, coherence='u_mass')
 	coherence = cm.get_coherence()
+	perplexity = ldamodel.log_perplexity(corpus)
 	print("Cpherence",coherence)
-	print('\nPerplexity: ', model.log_perplexity(corpus))
-	return ldamodel, dictionary, coherence, model.log_perplexity(corpus), corpus
+	print('\nPerplexity: ', perplexity)
+	return ldamodel, coherence, perplexity
 	#perplex가 낮을수록, coherence가 높을수록 좋음
 	#https://coredottoday.github.io/2018/09/17/%EB%AA%A8%EB%8D%B8-%ED%8C%8C%EB%9D%BC%EB%AF%B8%ED%84%B0-%ED%8A%9C%EB%8B%9D/
 
-def csv_learn(file):
-	corpus = []
-	dictionary = corpora.Dictionary()
-	print("Model Learning Start...")
-	print("CSV File Preprocessing")
-	print("Topics(", NUM_TOPICS,"개)")
-	df = pd.read_csv(file)
-	#CSV 파일 형식에 따라 변경 필요
-	df['text'] = df.apply(lambda x: get_tk(df['title'] + df['text']))
-	tokenized_doc = df['text']
-	dictionary.add_documents(tokenized_doc)
-	corpus += [dictionary.doc2bow(text) for text in tokenized_doc]
-	print("Training...")
-	ldamodel = LdaMulticore(
-				corpus,
-				num_topics = NUM_TOPICS,
-				id2word = dictionary,
-				passes = PASSES,
-				workers = WORKERS
-				)
-	cm = CoherenceModel(model=model, corpus=corpus, coherence='u_mass')
-	coherence = cm.get_coherence()
-	print("Cpherence",coherence)
-	print('\nPerplexity: ', model.log_perplexity(corpus))
-	return ldamodel, dictionary, coherence, model.log_perplexity(corpus)
 
+## 특정 수만큼 포스트 가져오기
 def get_posts_df(coll, start, count, update = False):
-	# if update:
-	# 	posts = coll.find({{"learn" : 0}}).skip(start).limit(count)
-	# else:
-	# 	posts = coll.find().skip(start).limit(count)
-	posts = coll.find().skip(start).limit(count)
+	if update:
+		posts = coll.find({{"learn" : 0}}).skip(start).limit(count)
+	else:
+		posts = coll.find().skip(start).limit(count)
 	df = pd.DataFrame(columns = ["text"])
 	for post in posts:
 		if len(post['post'] + post['title']) < POST_LIMIT:
@@ -117,9 +100,10 @@ def get_posts_df(coll, start, count, update = False):
 		token = post['token'] + post['tag']
 		temp = pd.DataFrame({"text":[token]})
 		df = df.append(temp, ignore_index = True)
-		print("OK")
+		#print("OK")
 	return df
 
+# 모델 저장하기
 def save_model(ldamodel, dictionary, model_path = model_path, dict_path = dict_path):
 	ldamodel.save(datepath(model_path))
 	dictionary.save(dict_path)
@@ -127,12 +111,15 @@ def save_model(ldamodel, dictionary, model_path = model_path, dict_path = dict_p
 
 ############################################
 # 모델 활용 UTIL 함수
+
+## 모델 불러오기
 def load_model():
 	dictionary = corpora.Dictionary.load(model_path)
-	lda = LdaModel.load(datepath(model_path))
+	lda = LdaModel.load(datapath(model_path))
 	print("loaded")
 	return lda, dictionary
 
+## 모델의 모든 토픽 정보 출력
 def show_topics(ldamodel, num_words = 5):
 	topics = ldamodel.print_topics(
 		num_words = num_words) # 토픽 단어 제한
@@ -142,6 +129,7 @@ def show_topics(ldamodel, num_words = 5):
 		print(topic)
 	return topics
 
+## 하나의 문서에 대하여 토픽 정보 예측
 def get_topics(ldamodel, dictionary, doc, is_string):
 	df = pd.DataFrame({'text':[doc]})
 	if is_string:
@@ -154,6 +142,8 @@ def get_topics(ldamodel, dictionary, doc, is_string):
 		temp = sorted(topic_list, key = lambda x: (x[1]), reverse=True)
 		return temp
 
+
+## 해당 단어리스트가 딕셔너리에 내에 포함된 단어인지 검증
 def is_vaild_words(dict, word_list):
 	temp = dict.doc2idx(word_list)
 	result = []
@@ -164,6 +154,7 @@ def is_vaild_words(dict, word_list):
 			result += [True]
 	return result
 
+## 모델 시각화
 def visualization(ldamodel, corpus, dictionary):
 	print("graphing...")
 	vis = pyLDAvis.gensim.prepare(ldamodel, corpus, dictionary)
@@ -174,3 +165,11 @@ def visualization(ldamodel, corpus, dictionary):
 
 def get_time():
 	print("WorkingTime: {} sec".format(round(time.time()-start,3)))
+
+
+###################################################################
+# 테스트용 함수
+
+
+
+
